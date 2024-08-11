8b20d3f41a6cf159762324a296d6ac8290442331
letsCodeWithHasib <mohdhasib2001@gmail.com>
2024-08-11 17:25:30 +0530

    Creating a Navbar

--- cobra-ecom/src/app/page.tsx
+++ cobra-ecom/src/app/page.tsx
@@ -1,5 +1,6 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { Check } from "lucide-react";
+import { Check, Star } from "lucide-react";
+import Phone from "@/components/Phone";
 
 export default function Home() {
   return (
@@ -63,14 +64,40 @@ export default function Home() {
                     alt="user image"
                   />
                   <img
-                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    className="inline-block object-cover h-10 w-10 rounded-full ring-2 ring-slate-100"
                     src="/users/user-5.jpg"
                     alt="user image"
                   />
                 </div>
+                <div className="flex flex-col justify-between items-center sm:items-start">
+                  <div className="flex gap-0.5">
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                  </div>
+                  <p>
+                    <span className="font-semibold">1.250 </span>Happy Customers
+                  </p>
+                </div>
               </div>
+            </div>
+          </div>
 
-              <div className="flex gap-.5"></div>
+          <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit">
+            <div className="relative md:max-w-xl">
+              <img
+                src="/your-image.png"
+                className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block"
+                alt=""
+              />
+              <img
+                src="/line.png"
+                className="absolute w-20 -left-6 -bottom-6 select-none"
+                alt=""
+              />
+              <Phone className="w-64" imgSrc="/testimonials/1.jpg" />
             </div>
           </div>
         </MaxWidthWrapper>
