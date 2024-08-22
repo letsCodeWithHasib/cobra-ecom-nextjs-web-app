6945118becc75a6bce0b546ac1e44ef651eceb24
letsCodeWithHasib <mohdhasib2001@gmail.com>
2024-08-22 16:43:54 +0530

    Creating a call to action section

--- cobra-ecom/src/app/page.tsx
+++ cobra-ecom/src/app/page.tsx
@@ -1,6 +1,7 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { Check, Star } from "lucide-react";
 import Phone from "@/components/Phone";
+import { Icons } from "@/components/Icons";
 
 export default function Home() {
   return (
@@ -98,6 +99,40 @@ export default function Home() {
                 alt=""
               />
               <Phone className="w-64" imgSrc="/testimonials/1.jpg" />
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+
+      {/* value proposition section */}
+      <section className="bg-slate-100 py-24">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+          <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+            <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+              What our{" "}
+              <span className="relative px-2">
+                customers{" "}
+                <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500" />
+              </span>{" "}
+              say
+            </h2>
+            <img
+              src="/snake-2.png"
+              alt=""
+              className="w-24 order-0 lg:order-2"
+            />
+          </div>
+
+          <div className="mx-auto gird max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-3">
+                <Star className="h-5 w-5 bg-green-600 fill-green-600" />
+                <Star className="h-5 w-5 bg-green-600 fill-green-600" />
+                <Star className="h-5 w-5 bg-green-600 fill-green-600" />
+                <Star className="h-5 w-5 bg-green-600 fill-green-600" />
+                <Star className="h-5 w-5 bg-green-600 fill-green-600" />
+              </div>
+              <div></div>
             </div>
           </div>
         </MaxWidthWrapper>
